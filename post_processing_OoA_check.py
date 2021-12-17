4f2dae874a5d6cc3493090e13dfe7d647b7018a8
jbrillon <julien.brillon@mail.mcgill.ca>
2021-12-17 15:01:23 -0500

    tabs to spaces

--- post_processing_OoA_check.py
+++ post_processing_OoA_check.py
@@ -22,109 +22,109 @@ components_avail = np.array([0,1],dtype=int)
 
 print('=====================================================')
 for j in components_avail:
-	figure_title = "Root-Mean-Square of Error vs Inverse of Spatial Grid Spacing"
-	figure_title_print = "Root-Mean-Square of Error vs Inverse of Spatial Grid Spacing"
-	print('Plotting: ' + figure_title_print)
-	fig = plt.figure(figure_title)
-	plt.grid()
-	plt.title(figure_title,fontsize=12)#,fontweight='bold')
-	plt.xlabel(r'$h^{-1} = (\sqrt{\Delta x\Delta y})^{-1}$',fontsize=12)
-	plt.ylabel(r"$\epsilon^{RMS}_{TR}$",rotation=90,fontsize=12)
+    figure_title = "Root-Mean-Square of Error vs Inverse of Spatial Grid Spacing"
+    figure_title_print = "Root-Mean-Square of Error vs Inverse of Spatial Grid Spacing"
+    print('Plotting: ' + figure_title_print)
+    fig = plt.figure(figure_title)
+    plt.grid()
+    plt.title(figure_title,fontsize=12)#,fontweight='bold')
+    plt.xlabel(r'$h^{-1} = (\sqrt{\Delta x\Delta y})^{-1}$',fontsize=12)
+    plt.ylabel(r"$\epsilon^{RMS}_{TR}$",rotation=90,fontsize=12)
 
-	for i in terms_avail:
-		# dx
-		subdirectory = subdirectories[0]
-		filename = "dx_store_%s_%s_%s" % (terms[i],components[j],eqn)
-		dx_store = np.loadtxt(subdirectory+filename+'.'+data_fileType,unpack=False)
-		# RMS error
-		filename = "RMS_error_dx_%s_%s_%s" % (terms[i],components[j],eqn)
-		RMS_store_dx = np.loadtxt(subdirectory+filename+'.'+data_fileType,unpack=False)
-		
-		# plt.xlim([1e0,1e3])
-		# max_val_current = np.max(RMS_store_dx)
-		# max_val = np.min([max_val_current,np.max(RMS_store_dx)])
-		# min_val_current = np.min(RMS_store_dx)
-		# min_val = np.min([min_val_current,np.min(RMS_store_dx)])
-		# plt.ylim([min_val,max_val])
-		
-		# testing
-		plt.xlim([1e0,1e2])
-		# plt.ylim([1e-3,1e0])
+    for i in terms_avail:
+        # dx
+        subdirectory = subdirectories[0]
+        filename = "dx_store_%s_%s_%s" % (terms[i],components[j],eqn)
+        dx_store = np.loadtxt(subdirectory+filename+'.'+data_fileType,unpack=False)
+        # RMS error
+        filename = "RMS_error_dx_%s_%s_%s" % (terms[i],components[j],eqn)
+        RMS_store_dx = np.loadtxt(subdirectory+filename+'.'+data_fileType,unpack=False)
+        
+        # plt.xlim([1e0,1e3])
+        # max_val_current = np.max(RMS_store_dx)
+        # max_val = np.min([max_val_current,np.max(RMS_store_dx)])
+        # min_val_current = np.min(RMS_store_dx)
+        # min_val = np.min([min_val_current,np.min(RMS_store_dx)])
+        # plt.ylim([min_val,max_val])
+        
+        # testing
+        plt.xlim([1e0,1e2])
+        # plt.ylim([1e-3,1e0])
 
-		name = term_names[j]
-		plt.loglog(1.0/dx_store,RMS_store_dx,label=name)
-	
-	# Confirm the order of accuracy
-	for n in range(1,4):
-		if j==0:
-			name = r"$\Delta x^{-%i}$" % n
-		elif j==1:
-			name = r"$\Delta y^{-%i}$" % n
+        name = term_names[j]
+        plt.loglog(1.0/dx_store,RMS_store_dx,label=name)
+    
+    # Confirm the order of accuracy
+    for n in range(1,4):
+        if j==0:
+            name = r"$\Delta x^{-%i}$" % n
+        elif j==1:
+            name = r"$\Delta y^{-%i}$" % n
 
-		shift = -4.8
+        shift = -4.8
 
-		dx_n = (dx_store**n)*10.0**(shift+float(n))
-		plt.loglog(1.0/dx_store,dx_n,label=name,linewidth=0.5,linestyle='--')
+        dx_n = (dx_store**n)*10.0**(shift+float(n))
+        plt.loglog(1.0/dx_store,dx_n,label=name,linewidth=0.5,linestyle='--')
 
-	plt.tight_layout()
-	leg = plt.legend(loc='best', ncol=2, shadow=True, fancybox=True, fontsize=8)
-	print(' ... Saving figure ...')
-	if j==0:
-		figure_name = "RMS_error_" + terms[i] + "_x_" + eqn
-	elif j==1:
-		figure_name = "RMS_error_" + terms[i] + "_y_" + eqn
-	figure_fileType = 'png'
-	subdirectory = subdirectories[1]
-	plt.savefig(subdirectory + figure_name + '.' + figure_fileType,format=figure_fileType,dpi=500)
-	plt.close()
-	
-	# print('-----------------------------------------------------')
-	
-	# figure_title = "Root-Mean-Square of Error vs Inverse of Temporal Grid Spacing\n for %s Strategies" % (temp_discs[i])
-	# figure_title_print = "Root-Mean-Square of Error vs Inverse of Temporal Grid Spacing for %s Strategies" % (temp_discs[i])
-	# print('Plotting: ' + figure_title_print)
-	# fig = plt.figure(figure_title)
-	# plt.grid()
-	# plt.title(figure_title,fontsize=12,fontweight='bold')
-	# plt.xlabel(r'$\Delta t^{-1}$',fontsize=12)
-	# plt.ylabel(r"$\epsilon^{RMS}_{TR}$",rotation=90,fontsize=12)
+    plt.tight_layout()
+    leg = plt.legend(loc='best', ncol=2, shadow=True, fancybox=True, fontsize=8)
+    print(' ... Saving figure ...')
+    if j==0:
+        figure_name = "RMS_error_" + terms[i] + "_x_" + eqn
+    elif j==1:
+        figure_name = "RMS_error_" + terms[i] + "_y_" + eqn
+    figure_fileType = 'png'
+    subdirectory = subdirectories[1]
+    plt.savefig(subdirectory + figure_name + '.' + figure_fileType,format=figure_fileType,dpi=500)
+    plt.close()
+    
+    # print('-----------------------------------------------------')
+    
+    # figure_title = "Root-Mean-Square of Error vs Inverse of Temporal Grid Spacing\n for %s Strategies" % (temp_discs[i])
+    # figure_title_print = "Root-Mean-Square of Error vs Inverse of Temporal Grid Spacing for %s Strategies" % (temp_discs[i])
+    # print('Plotting: ' + figure_title_print)
+    # fig = plt.figure(figure_title)
+    # plt.grid()
+    # plt.title(figure_title,fontsize=12,fontweight='bold')
+    # plt.xlabel(r'$\Delta t^{-1}$',fontsize=12)
+    # plt.ylabel(r"$\epsilon^{RMS}_{TR}$",rotation=90,fontsize=12)
 
-	# for j in lin_adv_disc_avail:
-	# 	# dx
-	# 	subdirectory = subdirectories[0]
-	# 	filename = "dt_store_method_%i_%i" % (i,j)
-	# 	dt_store = np.loadtxt(subdirectory+filename+'.'+data_fileType,unpack=False)
-	# 	# RMS error
-	# 	filename = "RMS_error_dt_method_%i_%i" % (i,j)
-	# 	RMS_store_dt = np.loadtxt(subdirectory+filename+'.'+data_fileType,unpack=False)
-		
-	# 	# plt.xlim([1e0,1e3])
-	# 	plt.ylim([1e-3,1e0])
-	# 	name = temp_discs[i] + ' with ' + lin_adv_discs[j]
-	# 	plt.loglog(1.0/dt_store,RMS_store_dt,label=name)
-	
-	# # Confirm the order of accuracy
-	# n_time = [1,2,3]
-	# for n in n_time: 
-	# 	name = r"$\Delta t^{-%i}$" % n
-	# 	if n == 2:
-	# 		shift = 2.0
-	# 	else:
-	# 		shift = 0.0
-	# 	if n == 3:
-	# 		shift = 3.0
-	# 	dt_n = (dt_store**n)*10.0**(shift+float(n))
-	# 	plt.loglog(1.0/dt_store,dt_n,label=name,linewidth=0.5,linestyle='--')
+    # for j in lin_adv_disc_avail:
+    #   # dx
+    #   subdirectory = subdirectories[0]
+    #   filename = "dt_store_method_%i_%i" % (i,j)
+    #   dt_store = np.loadtxt(subdirectory+filename+'.'+data_fileType,unpack=False)
+    #   # RMS error
+    #   filename = "RMS_error_dt_method_%i_%i" % (i,j)
+    #   RMS_store_dt = np.loadtxt(subdirectory+filename+'.'+data_fileType,unpack=False)
+        
+    #   # plt.xlim([1e0,1e3])
+    #   plt.ylim([1e-3,1e0])
+    #   name = temp_discs[i] + ' with ' + lin_adv_discs[j]
+    #   plt.loglog(1.0/dt_store,RMS_store_dt,label=name)
+    
+    # # Confirm the order of accuracy
+    # n_time = [1,2,3]
+    # for n in n_time: 
+    #   name = r"$\Delta t^{-%i}$" % n
+    #   if n == 2:
+    #       shift = 2.0
+    #   else:
+    #       shift = 0.0
+    #   if n == 3:
+    #       shift = 3.0
+    #   dt_n = (dt_store**n)*10.0**(shift+float(n))
+    #   plt.loglog(1.0/dt_store,dt_n,label=name,linewidth=0.5,linestyle='--')
 
-	# plt.tight_layout()
-	# leg = plt.legend(loc='best', ncol=2, shadow=True, fancybox=True, fontsize=8)
-	# print(' ... Saving figure ...')
-	# figure_name = "RMS_error_dt"
-	# figure_fileType = 'eps'
-	# subdirectory = subdirectories[1]
-	# plt.savefig(subdirectory + figure_name + '.' + figure_fileType,format=figure_fileType,dpi=500)
-	# plt.close()
-	# print('-----------------------------------------------------')
+    # plt.tight_layout()
+    # leg = plt.legend(loc='best', ncol=2, shadow=True, fancybox=True, fontsize=8)
+    # print(' ... Saving figure ...')
+    # figure_name = "RMS_error_dt"
+    # figure_fileType = 'eps'
+    # subdirectory = subdirectories[1]
+    # plt.savefig(subdirectory + figure_name + '.' + figure_fileType,format=figure_fileType,dpi=500)
+    # plt.close()
+    # print('-----------------------------------------------------')
 print('=====================================================')
 
 
--- spy.py
+++ spy.py
@@ -15,20 +15,20 @@ subdirectories = ['Data/','Figures/SPY/']
 #=====================================================
 # set the colormap and centre the colorbar
 class MidpointNormalize(colors.Normalize):
-	"""
-	Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
+    """
+    Normalise the colorbar so that diverging bars work there way either side from a prescribed midpoint value)
 
-	e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
-	"""
-	def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
-		self.midpoint = midpoint
-		colors.Normalize.__init__(self, vmin, vmax, clip)
+    e.g. im=ax1.imshow(array, norm=MidpointNormalize(midpoint=0.,vmin=-100, vmax=100))
+    """
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        colors.Normalize.__init__(self, vmin, vmax, clip)
 
-	def __call__(self, value, clip=None):
-		# I'm ignoring masked values and all kinds of edge cases to make a
-		# simple example...
-		x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
-		return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
+    def __call__(self, value, clip=None):
+        # I'm ignoring masked values and all kinds of edge cases to make a
+        # simple example...
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y), np.isnan(value))
 #=====================================================
 # Load matrix A
 BCs = ['Periodic','ChannelFlow'] #['Dirichlet','Neumann']
@@ -40,32 +40,32 @@ eqn = 'ghostCell'
 i = 2 # select operator (index)
 
 if i == 2:
-	matrix_shape = 'rectangular'
-	BC = BCs[1]
+    matrix_shape = 'rectangular'
+    BC = BCs[1]
 elif i == 1:
-	matrix_shape = 'square'
-	BC = BCs[0]
+    matrix_shape = 'square'
+    BC = BCs[0]
 
 if i < 3:
-	if matrix_shape == 'rectangular' and eqn == 'ymomentum':
-		Nxc = 4
-		Nyc = 4
-	else:
-		if eqn == 'ymomentum':
-			Nxc = 4
-			Nyc = 3
-		elif eqn == 'ghostCell' or eqn == 'boundary':
-			Nxc = 4
-			Nyc = 5
-		else:
-			Nxc = 4
-			Nyc = 4
+    if matrix_shape == 'rectangular' and eqn == 'ymomentum':
+        Nxc = 4
+        Nyc = 4
+    else:
+        if eqn == 'ymomentum':
+            Nxc = 4
+            Nyc = 3
+        elif eqn == 'ghostCell' or eqn == 'boundary':
+            Nxc = 4
+            Nyc = 5
+        else:
+            Nxc = 4
+            Nyc = 4
 elif i == 3 or i == 5 or i == 7:
-	Nxc = 5
-	Nyc = 4
+    Nxc = 5
+    Nyc = 4
 elif i == 4 or i == 6 or i == 8:
-	Nxc = 4
-	Nyc = 5	
+    Nxc = 4
+    Nyc = 5 
 
 print('=====================================================')
 # Load matrix A
@@ -84,28 +84,28 @@ mid_val=0
 cmap=matplotlib.cm.RdBu_r
 plt.imshow(A, cmap=cmap, clim=(elev_min, elev_max), norm=MidpointNormalize(midpoint=mid_val,vmin=elev_min, vmax=elev_max))
 if matrix_shape == 'rectangular':
-	plt.colorbar(label='Operator Weights',fraction=0.035, pad=0.04)
+    plt.colorbar(label='Operator Weights',fraction=0.035, pad=0.04)
 else:
-	plt.colorbar(label='Operator Weights')
+    plt.colorbar(label='Operator Weights')
 
 if matrix_shape == 'rectangular':
-	if eqn == 'ymomentum':
-		plt.xticks(np.arange(-0.5,Nxc*Nyc),0*range(0, Nxc*Nyc))
-		plt.xlim(-0.5,Nxc*Nyc-0.5)
-		plt.yticks(np.arange(-0.5,Nxc*(Nyc-1)),0*range(0, Nxc*(Nyc-1)+1))
-	elif eqn == 'ghostCell':
-		plt.xticks(np.arange(-0.5,Nxc*(Nyc+1)),0*range(0, Nxc*Nyc))
-		plt.xlim(-0.5,Nxc*(Nyc+1)-0.5)
-		plt.yticks(np.arange(-0.5,Nxc*Nyc),0*range(0,Nxc*Nyc))
-		plt.ylim(Nxc*Nyc-0.5,-0.5)
-		# plt.ylim(-0.5,Nxc*(Nyc+1)-0.5)
-	else:
-		plt.xticks(np.arange(-0.5,Nxc*(Nyc+1)),0*range(0, Nxc*Nyc))
-		plt.xlim(-0.5,Nxc*(Nyc+1)-0.5)
-		plt.yticks(np.arange(-0.5,Nxc*Nyc),0*range(0, Nxc*Nyc+1))
+    if eqn == 'ymomentum':
+        plt.xticks(np.arange(-0.5,Nxc*Nyc),0*range(0, Nxc*Nyc))
+        plt.xlim(-0.5,Nxc*Nyc-0.5)
+        plt.yticks(np.arange(-0.5,Nxc*(Nyc-1)),0*range(0, Nxc*(Nyc-1)+1))
+    elif eqn == 'ghostCell':
+        plt.xticks(np.arange(-0.5,Nxc*(Nyc+1)),0*range(0, Nxc*Nyc))
+        plt.xlim(-0.5,Nxc*(Nyc+1)-0.5)
+        plt.yticks(np.arange(-0.5,Nxc*Nyc),0*range(0,Nxc*Nyc))
+        plt.ylim(Nxc*Nyc-0.5,-0.5)
+        # plt.ylim(-0.5,Nxc*(Nyc+1)-0.5)
+    else:
+        plt.xticks(np.arange(-0.5,Nxc*(Nyc+1)),0*range(0, Nxc*Nyc))
+        plt.xlim(-0.5,Nxc*(Nyc+1)-0.5)
+        plt.yticks(np.arange(-0.5,Nxc*Nyc),0*range(0, Nxc*Nyc+1))
 else:
-	plt.xticks(np.arange(-0.5,Nxc*Nyc),0*range(0, Nxc*Nyc+1))
-	plt.yticks(np.arange(-0.5,Nxc*Nyc),0*range(0, Nxc*Nyc+1))
+    plt.xticks(np.arange(-0.5,Nxc*Nyc),0*range(0, Nxc*Nyc+1))
+    plt.yticks(np.arange(-0.5,Nxc*Nyc),0*range(0, Nxc*Nyc+1))
 plt.grid()
 plt.tight_layout()
 print('... Saving figure ...')
